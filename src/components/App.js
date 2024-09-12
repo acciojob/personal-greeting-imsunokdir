b2aaa9299377628aa444d7102ee46ecbffefff49
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-09-12 11:20:41 +0530

    add input field and display text in another div on onInput

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const handleInput = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label for="name">Enter your name: </label>
+      <br></br>
+      <input id="name" onInput={handleInput}></input>
+      <div>{name}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
